46aa3d08a24db047b6030cce7288e3ad59003dc0
genimi-huang <710693662@qq.com>
2019-06-26 13:59:59 +0800

    添加分类功能

--- src/components/categories/categories.vue
+++ src/components/categories/categories.vue
@@ -1,6 +1,7 @@
 <template>
   <el-card>
     <Breadcrumbs level2="商品管理" level3="商品分类"></Breadcrumbs>
+    <el-button plain type="success" @click="addCategory">添加分类</el-button>
     <el-table
       :data="pageList"
       style="width: 100%"
@@ -51,13 +52,35 @@
       :total="total"
     >
     </el-pagination>
+    <!-- 对话框 -->
+    <el-dialog title="添加分类" :visible.sync="dialogFormVisible">
+      <el-form>
+        <el-form-item label="分类名称" :label-width="formLabelWidth">
+          <el-input v-model="categoryName" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="分类名称" :label-width="formLabelWidth">
+          <el-cascader
+            v-model="selectVal"
+            :options="options"
+            clearable
+            :props="propsObj"
+          ></el-cascader>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategoryFn"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
 <script>
 import Breadcrumbs from '@/components/common/breadcrumb'
 export default {
-  data () {
+  data() {
     return {
       categoryList: [],
       total: 0,
@@ -66,14 +89,25 @@ export default {
       pagesizes: [5, 10, 15],
       loading: true,
       // 手动分页
-      pageList: []
+      pageList: [],
+      dialogFormVisible: false,
+      formLabelWidth: '80px',
+      options: [],
+      selectVal: [],
+      propsObj: {
+        expandTrigger: 'hover',
+        label: 'cat_name',
+        value: 'cat_id',
+        checkStrictly: true
+      },
+      categoryName:''
     }
   },
   components: {
     Breadcrumbs
   },
   methods: {
-    getTableList () {
+    getTableList() {
       this.$http({
         method: 'get',
         // url: `categories?type=3&pagenum=${this.pagenum}&pagesize=${this.pagesize}`
@@ -92,16 +126,55 @@ export default {
         }
       })
     },
-    sizeChange (size) {
+    sizeChange(size) {
       this.pagesize = size
       this.getTableList()
     },
-    currentChange (currentPage) {
+    currentChange(currentPage) {
       this.pagenum = currentPage
       this.getTableList()
+    },
+    addCategory() {
+      this.dialogFormVisible = true
+      this.$http({
+        method: 'get',
+        url: `categories?type=2`
+      }).then(res => {
+        let {meta, data} = res.data
+        if(meta.status === 200) {
+          this.options = data
+        }else {
+          this.$message.error(meta.msg)
+        }
+      })
+    },
+    addCategoryFn() {
+      let pid = this.selectVal.length === 0 ? 0 : this.selectVal[this.selectVal.length - 1]
+      let level = this.selectVal.length
+      this.$http({
+        method: 'post',
+        url: `categories?type=2`,
+        data: {
+          cat_pid: pid,
+          cat_name: this.categoryName,
+          cat_level: level
+        }
+      }).then(res =>{
+        let {meta} = res.data
+        if(meta.status === 201) {
+          this.$message({
+            message: meta.msg,
+            type: 'success'
+          })
+          this.dialogFormVisible = false
+          this.getTableList()
+        }else {
+          this.$message.error(meta.msg)
+        }
+      })
     }
   },
-  mounted () {
+  mounted() {
     this.getTableList()
   }
 }
