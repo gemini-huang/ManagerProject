2e8c5e5615fd75ff15c01a9184ff998d8a43a06a
genimi-huang <710693662@qq.com>
2019-06-26 11:52:34 +0800

    手动设置分页功能

--- src/components/rights/rights.vue
+++ src/components/rights/rights.vue
@@ -1,7 +1,7 @@
 <template>
   <el-card>
     <Breadcrumb level2="权限管理" level3="权限列表"></Breadcrumb>
-    <el-table :data="tableData" border style="width: 100%" class="mytable" v-loading="loading">
+    <el-table :data="pageList" border style="width: 100%" class="mytable" v-loading="loading">
       <el-table-column type="index" width="40"> </el-table-column>
       <el-table-column prop="authName" label="权限名称" width="200"> </el-table-column>
       <el-table-column prop="path" label="路径" width="200"> </el-table-column>
@@ -13,6 +13,17 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页区域 -->
+    <el-pagination
+      @size-change="sizeChange"
+      @current-change="currentChange"
+      :current-page="pagenum"
+      :page-sizes="pagesizes"
+      :page-size="pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    >
+    </el-pagination>
   </el-card>
 </template>
 
@@ -22,7 +33,13 @@ export default {
   data () {
     return {
       tableData: [],
-      loading: true
+      loading: true,
+      // 分页数据
+      pageList: [],
+      pagenum: 1,
+      pagesize: 5,
+      pagesizes:[5,10,15],
+      total: 0
     }
   },
   components: {
@@ -38,8 +55,35 @@ export default {
         if (meta.status === 200) {
           this.loading = false
           this.tableData = data
+          this.total = data.length
+          // 手动设置分页功能
+          /*
+            页容量 5
+            当前页 1  0 ~ 4
+            当前页 2  5 ~ 9
+            当前页 3  10 ~ 14
+            ......
+            规律
+              5(1 - 1) ~ 5 * 1 - 1
+              pagesize(pagenum - 1) ~ pagesize * pagenum
+              let begin = pagesize*(pagenum - 1)
+              let end = pagesize * pagenum - 1
+              然后使用数组截取的方法 slice() 这个方法不会影响原来的数组,截取的区间是左闭右开区间，不包括最后一位
+              this.pageList = this.tableData.slice(begin, end + 1)
+          */
+          let begin = this.pagesize * (this.pagenum - 1)
+          let end = this.pagesize * this.pagenum - 1
+          this.pageList = this.tableData.slice(begin, end + 1)
         }
       })
+    },
+    sizeChange(size) {
+      this.pagesize = size
+      this.getTableData()
+    },
+    currentChange(currentPage) {
+      this.pagenum = currentPage
+      this.getTableData()
     }
   },
   mounted () {
